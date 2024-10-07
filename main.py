84d140c15f8491ee403f04fa40ad54589654e0e3
Samuele Barzaghi <sambarza@gmail.com>
2024-10-07 22:06:45 +0200

    Fix called two times if reload true

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from world import world
 import uvicorn
 
-print("HERE MAIN")
+print("HERE MAIN", __name__)
+
 if __name__ == "__main__":
+
     print("main")
-    uvicorn.run("main:world.fastapi_app", host="127.0.0.1", port=5000, reload=False)
+
+    uvicorn.run(
+        "world:world.fastapi_app",
+        host="127.0.0.1",
+        port=5000,
+        reload=True,
+        reload_dirs="world",
+    )
